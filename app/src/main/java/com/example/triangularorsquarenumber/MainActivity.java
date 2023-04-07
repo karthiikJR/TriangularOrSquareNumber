348a5f90d56846c532f9be8580b40eaac7f00275
KarthikJR <karthikjrao69@gmail.com>
2023-04-07 11:04:33 +0530

    master

--- app/src/main/java/com/example/triangularorsquarenumber/MainActivity.java
+++ app/src/main/java/com/example/triangularorsquarenumber/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.triangularorsquarenumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Checksqrtri ck;
+
+    static class Checksqrtri{
+        int num;
+
+        public boolean isSqr(){
+            int root = (int) Math.sqrt(num);
+            return root * root == num;
+        }
+
+        public boolean isTri(){
+            int a = 0;
+            int flag = 0;
+            for(int i = 1;i <= num;i++){
+                a+=i;
+                if(a==num)
+                    flag = 1;
+
+            }
+            return flag == 1;
+        }
+
+    }
+
+    public void checkNum(View view){
+        TextView tv = findViewById(R.id.etInput);
+        int num = Integer.parseInt(tv.getText().toString());
+
+        Log.i("number", tv.getText().toString());
+
+        ck.num = num;
+        String message;
+
+        if(ck.isSqr() && ck.isTri()){
+            message = "It is both Square number and Triangular number";
+        }
+        else if(ck.isSqr())
+            message = "It is a Square number";
+        else if (ck.isTri())
+            message = "It is a Triangular number";
+        else
+            message = "It is not a Square number or a Triangular number";
+        Toast t = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.CENTER,0,0);
+        t.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ck = new Checksqrtri();
     }
 }
